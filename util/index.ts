72e91e5e53e1ccadeeed5481be0a070227763203
dottjt <julius.reade@myob.com>
2020-06-10 07:30:49 +1000

    similar

--- util/index.ts
+++ util/index.ts
@@ -7,7 +7,7 @@ import { data, media, website } from '@dottjt/datareade';
 const main = async () => {
   const { generateHugoMDFilePodcast } = website;
   const { generateModifiedRSS } = media
-  const { episodesTWD } = data;
+  const { episodesTNDD } = data;
 
   const PODCAST_WEBSITE = 'https://theneverfapdeluxedaily.juliusreade.com/';
   // const TWD_RSS_URL = "http://rss.castbox.fm/everest/aab82e46f0cd4791b1c8ddc19d5158c3.xml"; // (this is the NFD one.)
@@ -18,7 +18,7 @@ const main = async () => {
   const contentDirectory = path.join(__dirname, '../', 'content');
 
   await generateHugoMDFilePodcast({
-    episodes: episodesTWD,
+    episodes: episodesTNDD,
     contentDirectory,
     email: 'neverfapdeluxe@gmail.com'
   });
@@ -28,7 +28,7 @@ const main = async () => {
     newRSSURL: NEW_RSS_URL,
     outputRSSFile: OUTPUT_RSS_FILE,
     authorRSSField: '<author>neverfapdeluxe@gmail.com (Julius Reade)</author>',
-    episodeList: episodesTWD,
+    episodeList: episodesTNDD,
     podcastWebsite: PODCAST_WEBSITE
   });
 };
